69cd876c17db1e0fcd07cfe30056866470a6e767
unknown <tu7pgums7@code.edu.az>
2022-03-27 12:38:58 +0400

    TaskWeapon

---
+++ HomeWork/TaskWeapon/Models/Weapon.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Diagnostics;
+
+
+namespace TaskWeapon.Models
+{
+    internal class Weapon
+    {
+
+        private int _magazinesize { get; set; }
+        private int _currentbulletcount { get; set; }
+        private string _firemode { get; set; }
+
+
+        public int MagazineSize { get; set; }
+        public int CurrentbulletCount { get; set; }
+        public string FireMode { get; set; }
+
+        int numberofcalls = 1;
+        int fire = -1;
+        int count = 0;
+        int reload = 0;
+
+        // Shoot Method 
+        public void Shoot()
+        {
+            if (CurrentbulletCount > 0)
+            {
+
+                Console.WriteLine(--CurrentbulletCount);
+            }
+            else
+            {
+                Console.WriteLine("You don't have any bullet");
+            }
+        }
+
+        public void Fire()
+        {
+            fire = CurrentbulletCount - CurrentbulletCount;
+            Console.WriteLine("bullets were fired, the number of bullets in the comb :" + fire);
+        }
+
+        public int GetRemainBulletCount()
+        {
+            if (CurrentbulletCount >= 0)
+            {
+                return MagazineSize - CurrentbulletCount;
+            }
+            return 0;
+        }
+        public void Reload()
+        {
+
+            reload = count + CurrentbulletCount;
+            Console.WriteLine("comb filled :" + reload);
+        }
+        public void ChangeFireMode()
+        {
+            if (FireMode == "Automatic")
+            {
+                FireMode = "single";
+            }
+            else
+            {
+                FireMode = "Automatic";
+            }
+            Console.WriteLine(FireMode);
+        }
+        public void ChangeCapacity(int _capacity)
+        {
+            numberofcalls = _capacity;
+            Console.WriteLine($"Capacity changed :{numberofcalls}");
+        }
+        public void ChangeCount(int _count)
+        {
+            CurrentbulletCount = _count;
+            Console.WriteLine($"Count Changed :{CurrentbulletCount}");
+        }
+        public void ResetSecond()
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            fire = CurrentbulletCount - CurrentbulletCount;
+            Console.WriteLine("bullets were fired, the number of bullets in the comb :" + fire);
+            stopwatch.Stop();
+            TimeSpan ts = stopwatch.Elapsed;
+
+
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime);
+
+
+        }
+    }
+}
+
---
+++ HomeWork/TaskWeapon/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using TaskWeapon.Models;
+
+namespace TaskWeapon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Weapon weapon = new Weapon();
+
+            weapon.MagazineSize = 30;
+            weapon.CurrentbulletCount = 30;
+            weapon.FireMode = "single";
+     
+            int choice;
+
+            do
+            {
+                Console.WriteLine("Choose the command");
+                Console.WriteLine("0:Information\n" +
+                    "1:Shoot\n" +
+                    "2:Fire\n" +
+                    "3:GetRemainBulletCount\n" +
+                    "4:Reload\n" +
+                    "5:ChangeMode\n" +
+                    "6.Stop Program\n" +
+                    "7:Edit\n");
+
+               
+                int capacity = weapon.MagazineSize;
+                int count = weapon.CurrentbulletCount;
+                string mode = weapon.FireMode;
+                
+
+               choice = Convert.ToInt32(Console.Read());
+                switch (choice)
+                {
+
+                     
+                case 0:
+                    Console.WriteLine($"Bullet capacity :{capacity},bullet count :{count},Fire Mode :{mode}");
+                        break;
+                    case 1:
+                        
+                        weapon.Shoot();
+                      
+                        break;
+                    case 2:
+                       
+                        weapon.Fire();
+                      
+                        break;
+                    case 3:
+                        Console.WriteLine("_____________________________________________________");
+                        Console.WriteLine(weapon.GetRemainBulletCount());
+                        Console.WriteLine("_____________________________________________________");
+                        break;
+                    case 4:
+                        Console.WriteLine("_____________________________________________________");
+                        weapon.Reload();
+                        Console.WriteLine("_____________________________________________________");
+                        break;
+                    case 5:
+                        Console.WriteLine("_____________________________________________________");
+                        weapon.ChangeFireMode();
+                        Console.WriteLine("_____________________________________________________");
+                        break;
+                    case 6:
+                        Console.WriteLine("_____________________________________________________");
+                        Console.WriteLine("Program Stoped");
+                        Console.WriteLine("_____________________________________________________");
+                        break;
+                    case 7:
+                        Console.WriteLine("Change capacity\n Bullet Count\n Reset seconds");
+                        string edit = Console.ReadLine();
+                        if (edit == "Change capacity")
+                        {
+                            Console.WriteLine("Change Capacity");
+                            weapon.ChangeCapacity(int.Parse(Console.ReadLine()));
+
+                        }
+                        else if (edit == "Bullet Count")
+                        {
+                            Console.WriteLine("Change bullet count");
+                            weapon.ChangeCount(int.Parse(Console.ReadLine()));
+                        }
+                        else if (edit == "Reset seconds")
+                        {
+                            weapon.ResetSecond();
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("make the right choice");
+                        break;
+                }
+
+            
+            } while (choice != 6);
+        }
+    
+    }
+}
